bd167879d6f528f834f2d0fa560aad26edce34af
Teplotax <eq.tfreis@gmail.com>
2023-08-19 17:04:38 -0300

    Created constant_main.go

--- cmd/constant/constant_main.go
+++ cmd/constant/constant_main.go
@@ -2,7 +2,17 @@ package main
 
 import "fmt"
 
+const (
+	_ = iota + 5
+	catSpecialist
+	dogSpecialist
+	snakeSpecialist
+)
+
 func main() {
 	const myConst int = 42
 	fmt.Printf("myConst: %v %T\n", myConst, myConst)
+
+	var specialistType = catSpecialist
+	fmt.Printf("%v\n", specialistType == catSpecialist)
 }
