7dcf04e31fc9c64e601f4cc6b986ed9fec8103c4
Teplotax <eq.tfreis@gmail.com>
2023-08-23 08:52:56 -0300

    Updated func_main.go

--- cmd/func/func_main.go
+++ cmd/func/func_main.go
@@ -6,6 +6,27 @@ import (
 
 func main() {
 
+	//noPanic()
+	//doPanic()
+
+	// Accept any amount of int values
+	result := variadicArgument("Some text here", 1, 2, 3, 4, 5)
+	fmt.Printf("Printing the result again: %v\n", result)
+
+	// Anonimous function
+	func() {
+		fmt.Println("This is an anonimous function")
+	}()
+
+	// Function as a variable
+	f := func() {
+		fmt.Println("Invokes with f()")
+	}
+	fmt.Println("Do something else")
+	f()
+}
+
+func noPanic() {
 	// Dealing with errors without panic
 	d, err := divide(5.0, 2.0)
 	if err != nil {
@@ -13,8 +34,10 @@ func main() {
 		return
 	}
 	fmt.Println(d)
+}
 
-	d, err = divide(5.0, 0.0)
+func doPanic() {
+	d, err := divide(5.0, 0.0)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -27,4 +50,15 @@ func divide(a, b float64) (float64, error) {
 		return 0.0, fmt.Errorf("Cannot divide by zero")
 	}
 	return a / b, nil
+}
+
+// The variadic argument must be the last one
+func variadicArgument(text string, values ...int) int {
+	result := 0
+	for _, v := range values {
+		result += v
+	}
+	fmt.Println(text)
+	fmt.Printf("Sum of %v = %v\n", values, result)
+	return result
 }
